f80f435d936909f3f55fca9a024838769f7df792
xwy1130 <1197785029@qq.com>
2021-10-23 11:05:52 +0800

    add

--- client/src/pages/2021-10-23/index.tsx
+++ client/src/pages/2021-10-23/index.tsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import { useState, ReactElement, ReactNode } from 'react';
 import { Upload, Modal } from 'antd';
 import { v4 as uuidv4 } from 'uuid';
@@ -171,5 +172,44 @@ function renderVideo(file: any, actions: any): any {
         </div>
       </Modal>
     </span>
+=======
+import { useState } from 'react';
+import { Upload } from 'antd';
+// import ImgCrop from 'antd-img-crop';
+
+export default function Uploads(props: any) {
+  const onChange = ({ fileList: newFileList }: any) => {
+    console.log('fileList', newFileList);
+
+    // setFileList(newFileList);
+  };
+
+  // const onPreview = async file => {
+  //   let src = file.url;
+  //   if (!src) {
+  //     src = await new Promise(resolve => {
+  //       const reader = new FileReader();
+  //       reader.readAsDataURL(file.originFileObj);
+  //       reader.onload = () => resolve(reader.result);
+  //     });
+  //   }
+  //   const image = new Image();
+  //   image.src = src;
+  //   const imgWindow = window.open(src);
+  //   imgWindow.document.write(image.outerHTML);
+  // };
+
+  return (
+    <div>
+      <Upload
+        action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
+        listType="picture-card"
+        onChange={onChange}
+        // onPreview={onPreview}
+      >
+        上传图片
+      </Upload>
+    </div>
+>>>>>>> 1a38f51... add
   );
 }
