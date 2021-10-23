c7a26c5687e9a3cbc2c96ab267ea0df10754e3ef
xwy1130 <1197785029@qq.com>
2021-10-23 17:07:29 +0800

    upload

--- client/src/pages/2021-10-23/index.tsx
+++ client/src/pages/2021-10-23/index.tsx
@@ -1,4 +1,7 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> 23aa7dd... upload
 import { useState, ReactElement, ReactNode } from 'react';
 import { Upload, Modal } from 'antd';
 import { v4 as uuidv4 } from 'uuid';
@@ -11,6 +14,7 @@ const TYPES_VIDEO = ['mp4'];
 const TYPES_IMAGE = ['png', 'jpg', 'jpeg', 'bpm'];
 
 async function getBase64(file: any) {
+<<<<<<< HEAD
   return new Promise((resolve, reject) => {
     const reader = new FileReader();
     reader.readAsDataURL(file);
@@ -176,12 +180,45 @@ function renderVideo(file: any, actions: any): any {
 import { useState } from 'react';
 import { Upload } from 'antd';
 // import ImgCrop from 'antd-img-crop';
+=======
+  const image = new Image();
+  image.src = file?.url;
+  image.crossOrigin = 'anonymous';
+  image.onload = (ev) => {
+    image.onload = null;
+
+  }
+}
+>>>>>>> 23aa7dd... upload
 
 export default function Uploads(props: any) {
-  const onChange = ({ fileList: newFileList }: any) => {
-    console.log('fileList', newFileList);
+  const [ fileList, setFileList ] = useState<any>(() => [
+    {
+      uid: uuidv4(),
+      status: 'done',
+      name: '1634972484509587.mp4',
+      url: HTTP_PREFIX + '/public/tmp/6a2fe1d8-9a03-4528-9544-cc865dfb3d49-1634972484509587.mp4',
+      percent: 100,
+      response: {
+        success: true,
+        data: {
+          pathname: '/public/tmp/6a2fe1d8-9a03-4528-9544-cc865dfb3d49-1634972484509587.mp4',
+        }
+      }
+    }
+  ]);
 
-    // setFileList(newFileList);
+  const onChange = async ({ file, fileList: newFileList }: any) => {
+    setFileList(newFileList);
+
+    if (file.status !== 'done') {
+      return;
+    }
+
+    const type = getFileType(file.name);
+    if (TYPES_IMAGE.includes(type)) {
+      return;
+    }
   };
 
   // const onPreview = async file => {
@@ -196,20 +233,101 @@ export default function Uploads(props: any) {
   //   const image = new Image();
   //   image.src = src;
   //   const imgWindow = window.open(src);
-  //   imgWindow.document.write(image.outerHTML);
+  //   imgWindow?.document.write(image.outerHTML);
   // };
 
   return (
-    <div>
+    <div style={{ padding: 24 }}>
       <Upload
-        action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
+        action={`${HTTP_PREFIX}/api/upload`}
         listType="picture-card"
         onChange={onChange}
+        fileList={fileList}
+        name="File"
+        itemRender={itemRender}
         // onPreview={onPreview}
       >
         上传图片
       </Upload>
     </div>
 >>>>>>> 1a38f51... add
+  );
+}
+
+function getFileType(name: string): string {
+  return (name.replace(/[\S\s]+?\.([\w]+)$/, '$1') || '').toLowerCase();
+}
+
+function itemRender(originNode: any, file: any, fileList: any[], actions: any) {
+  const type = getFileType(file.name);
+  console.log('type', type);
+  if (TYPES_VIDEO.includes(type)) {
+    return renderVideo(file, actions);
+  }
+
+  return originNode;
+}
+
+function renderVideo(file: any, actions: any): any {
+  const [ visible, setVisible ] = useState<boolean>(false);
+  const openMask = () => setVisible(true);
+  const closeMask = () => setVisible(false);
+
+  const [ iconDelVisible, setIconDelVisible ] = useState<boolean>(false);
+  const showIconDel = () => setIconDelVisible(true);
+  const hideIconDel = () => setIconDelVisible(false);
+
+  const src = !file?.response ? '' : `${HTTP_PREFIX}${file?.response?.data?.pathname}`;
+  console.log('file', file);
+
+  const clsIconDel = classNames('video-element-icon-del', {
+    active: iconDelVisible
+  });
+
+  return (
+    <span
+      className='video-element'
+      onMouseEnter={showIconDel}
+      onMouseLeave={hideIconDel}
+    >
+      <span className='video-element-layer-video'>
+        {
+          !src ? null :
+          <video src={src} preload='preload'>
+            不支持video标签
+          </video>
+        }
+      </span>
+      <span className='video-element-layer-play'>
+        <PlayCircleOutlined />
+      </span>
+      <span
+        className='video-element-layer-mask'
+        onClick={openMask}
+      />
+      <span
+        className={clsIconDel}
+        onClick={(ev) => {
+          ev.stopPropagation();
+          actions.remove();
+        }}
+      >
+        <CloseOutlined />
+      </span>
+      <Modal
+        title='老妹儿的视频'
+        visible={visible}
+        onCancel={closeMask}
+        onOk={closeMask}
+        maskClosable
+        footer={null}
+      >
+        <div className='video-element-modal-mask'>
+          <video src={src} preload='preload' controls>
+            不支持video标签
+          </video>
+        </div>
+      </Modal>
+    </span>
   );
 }
