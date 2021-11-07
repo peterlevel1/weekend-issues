4043a5f25d144fb9fcb80817d0d7cd6900f74619
xwy1130 <1197785029@qq.com>
2021-11-07 17:24:06 +0800

    小徐 - 业务验收 - 上传

--- client/src/pages/2021-11-07/xx-upload.tsx
+++ client/src/pages/2021-11-07/xx-upload.tsx
@@ -6,6 +6,8 @@ import {
 } from 'antd';
 import { UploadOutlined, DeleteOutlined } from '@ant-design/icons';
 import { Uploads } from './servse';
+import './xx-upload.less';
+import classNames from 'classnames';
 
 // truthy: 类真值的 !!truthy -> true, 有东西的
 // falsy: 类价值的, !!falsy -> false, ('', false, 0, null, undefined, NaN)
@@ -42,96 +44,118 @@ export const normalizeFile = (e: any) => {
   return ret;
 };
 
+// export type OP_TYPE = 'update' | 'detail';
+
 export default function XXUpload(props: any) {
-  const { type, accept, form, fileList, onChange } = props;
+  const { opType, type, accept, form, fileList, onChange, detail } = props;
+  const cls = classNames('xx-upload', opType);
+
+  if (opType === 'detail') {
+    const list = type === 'picture' ? detail?.picture : detail?.video;
+    return (
+      <div className={cls}>
+        {
+          (list || []).map((item: any) => (
+            <div>
+              <p>{item.name}</p>
+            </div>
+          ))
+        }
+      </div>
+    );
+  }
 
   return (
-    <Upload
-      accept={accept}
-      fileList={fileList}
-      onChange={({ fileList }) => {
-        const arr = fileList.map(file => {
-          const url = file.url || file?.response?.result[0];
-          if (url && !file.url) {
-            file.url = url;
+    <div className={cls}>
+      <Upload
+        // listType={opType === 'update' ? 'text' : 'picture-card'}
+        listType='text'
+        accept={accept}
+        fileList={fileList}
+        onChange={({ fileList }) => {
+          const arr = fileList.map(file => {
+            const url = file.url || file?.response?.result[0];
+            if (url && !file.url) {
+              file.url = url;
+            }
+            return file;
+          });
+
+          onChange(arr);
+        }}
+        onDownload={(file) => {
+          const url = `${file.response.result[0]}`;
+          window.open(url);
+          return url;
+        }}
+        showUploadList={{
+          showDownloadIcon: true,
+          downloadIcon: (file) => (
+            <a
+              download
+              href={`${UPLOAD_PREFIX}${file.response.result[0]}`}
+              target='_blank'
+            >
+              download
+            </a>
+          ),
+          showRemoveIcon: true,
+          removeIcon: <DeleteOutlined onClick={e => console.log(e, 'custom removeIcon event')} />,
+        }}
+        customRequest={async (option: any) => {
+          const { file, onError, onSuccess } = option;
+          const value = form.getFieldValue(type);
+          console.log('customRequest: value', value);
+          console.log('customRequest: maxCount', maxCount);
+
+          const last = value[value.length - 1];
+
+          if (
+            value.length >= maxCount &&
+            (
+              last.response ||
+              last.percent > 0 ||
+              last.status === 'done' ||
+              last.status === 'pending' ||
+              last.status === 'error'
+            )
+          ) {
+            message.warn('最多上传' + maxCount + '张图片');
+            onError('');
+            return;
           }
-          return file;
-        });
 
-        onChange(arr);
-      }}
-      onDownload={(file) => {
-        const url = `${file.response.result[0]}`;
-        window.open(url);
-        return url;
-      }}
-      showUploadList={{
-        showDownloadIcon: true,
-        downloadIcon: (file) => (
-          <a
-            download
-            href={`${UPLOAD_PREFIX}${file.response.result[0]}`}
-            target='_blank'
-          >
-            download
-          </a>
-        ),
-        showRemoveIcon: true,
-        removeIcon: <DeleteOutlined onClick={e => console.log(e, 'custom removeIcon event')} />,
-      }}
-      customRequest={async (option: any) => {
-        const { file, onError, onSuccess } = option;
-        const value = form.getFieldValue(type);
-        console.log('customRequest: value', value);
-        console.log('customRequest: maxCount', maxCount);
+          const LT2M = value.reduce((memo: number, file: any) => {
+            const result = memo + file.size;
+            return result;
+          }, 0);
 
-        const last = value[value.length - 1];
+          console.log('LT2M', LT2M);
 
-        if (
-          value.length >= maxCount &&
-          (
-            last.response ||
-            last.percent > 0 ||
-            last.status === 'done' ||
-            last.status === 'pending' ||
-            last.status === 'error'
-          )
-        ) {
-          message.warn('最多上传' + maxCount + '张图片');
-          onError('');
-          return;
-        }
+          if (LT2M > (maxFileSize * 1024 * 1024)) {
+            message.warn('图片总大小不能超过' + maxFileSize + 'M');
+            onError('');
+            return;
+          }
 
-        const LT2M = value.reduce((memo: number, file: any) => {
-          const result = memo + file.size;
-          return result;
-        }, 0);
+          const formData = new FormData();
+          formData.append('file', file);
 
-        console.log('LT2M', LT2M);
-
-        if (LT2M > (maxFileSize * 1024 * 1024)) {
-          message.warn('图片总大小不能超过' + maxFileSize + 'M');
-          onError('');
-          return;
-        }
-
-        const formData = new FormData();
-        formData.append('file', file);
-
-        const resUpload = await Uploads(formData);
-        onSuccess(resUpload);
-      }}
-    >
-      <Button icon={<UploadOutlined />}>
-        {type === 'picture' ? '上传图片' : '上传视频'}
-      </Button>
-      <p style={{ fontSize: 12, color: '#9B9A9A', marginTop: 8 }}>
-        {
-          type === 'picture' ?
-            `支持扩展名：${accept} 大小20MB以内对图片文件，最多20张` :
-            `支持扩展名：${accept} 大小20MB以内对视频文件，最多10张`
-        }
-      </p>
-    </Upload>
+          const resUpload = await Uploads(formData);
+          onSuccess(resUpload);
+        }}
+      >
+        <Button icon={<UploadOutlined />}>
+          {type === 'picture' ? '上传图片' : '上传视频'}
+        </Button>
+        <p style={{ fontSize: 12, color: '#9B9A9A', marginTop: 8 }}>
+          {
+            type === 'picture' ?
+              `支持扩展名：${accept} 大小20MB以内对图片文件，最多20张` :
+              `支持扩展名：${accept} 大小20MB以内对视频文件，最多10张`
+          }
+        </p>
+      </Upload>
+    </div>
   )
 };
