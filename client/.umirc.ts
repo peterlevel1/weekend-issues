8ad642776288df1669671006af410f4e57db3640
xwy1130 <1197785029@qq.com>
2021-11-28 18:57:44 +0800

    remove 10-23

--- client/.umirc.ts
+++ client/.umirc.ts
@@ -10,7 +10,6 @@ export default defineConfig({
       component: '@/pages/index',
       routes: [
         { path: '/2021-10-17', component: '@/pages/2021-10-17' },
-        { path: '/2021-10-23', component: '@/pages/2021-10-23' },
         { path: '/2021-10-26', component: '@/pages/2021-10-26' },
         { path: '/2021-11-07', component: '@/pages/2021-11-07' },
         { path: '/2021-11-28', component: '@/pages/2021-11-28' },
